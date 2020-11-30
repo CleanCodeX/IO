36894f722e15c8ef25d72add28ff02b9b5ac38c3
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-11-30 21:18:54 +0100

    Added SramFile extensions for loading and setting offset value

--- Extensions/SramFileExtensions.cs
+++ Extensions/SramFileExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using Common.Shared.Min.Extensions;
 using Common.Shared.Min.Helpers;
 using SramCommons.Models;
 
@@ -8,6 +10,7 @@ namespace SramCommons.Extensions
 	{
 		public static void Load(this ISramFile source, string filepath)
 		{
+			source.ThrowIfNull(nameof(source));
 			Requires.FileExists(filepath, nameof(filepath));
 
 			source.Load(new FileStream(filepath, FileMode.Open, FileAccess.Read));
@@ -15,13 +18,53 @@ namespace SramCommons.Extensions
 
 		public static void Save(this ISramFile source, string filepath)
 		{
+			source.ThrowIfNull(nameof(source));
 			Requires.NotNullOrEmpty(filepath, nameof(filepath));
 
 			source.Save(new FileStream(filepath, FileMode.Create, FileAccess.Write));
 		}
 
+		public static uint GetOffsetValue(this ISramFile source, int offset, int length = 1)
+		{
+			source.ThrowIfNull(nameof(source));
+			Requires.LessThan(offset, source.SramBuffer.Length, nameof(offset));
+
+			var bytes = source.SramBuffer[offset..(offset+length+1)];
+
+			return BitConverter.ToUInt32(bytes);
+		}
+
+		public static void SetOffsetValue(this ISramFile source, int offset, byte value)
+		{
+			source.ThrowIfNull(nameof(source));
+			Requires.LessThan(offset, source.SramBuffer.Length, nameof(offset));
+
+			source.SramBuffer[offset] = value;
+		}
+
+		public static void SetOffsetValue(this ISramFile source, int offset, ushort value)
+		{
+			source.ThrowIfNull(nameof(source));
+			Requires.LessThan(offset, source.SramBuffer.Length, nameof(offset));
+
+			var bytes = BitConverter.GetBytes(value);
+
+			Array.Copy(source.SramBuffer, offset, bytes, 0, bytes.Length);
+		}
+
+		public static void SetOffsetValue(this ISramFile source, int offset, uint value)
+		{
+			source.ThrowIfNull(nameof(source));
+			Requires.LessThan(offset, source.SramBuffer.Length, nameof(offset));
+
+			var bytes = BitConverter.GetBytes(value);
+
+			Array.Copy(source.SramBuffer, offset, bytes, 0, bytes.Length);
+		}
+
 		public static void RawSave(this IRawSave source, string filepath)
 		{
+			source.ThrowIfNull(nameof(source));
 			Requires.NotNullOrEmpty(filepath, nameof(filepath));
 
 			source.RawSave(new FileStream(filepath, FileMode.Create, FileAccess.Write));
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -5,7 +5,7 @@ namespace SramCommons.Extensions
 {
 	public static class StringExtensions
 	{
-		private static readonly int _charSize = sizeof(char);
+		private const int CharSize = sizeof(char);
 
 		public static unsafe byte[] GetBytes(string str)
 		{
@@ -13,17 +13,17 @@ namespace SramCommons.Extensions
 			if (str.Length == 0) return Array.Empty<byte>();
 
 			fixed (char* p = str)
-				return new Span<byte>(p, str.Length * _charSize).ToArray();
+				return new Span<byte>(p, str.Length * CharSize).ToArray();
 		}
 
 		public static unsafe string GetString(byte[] bytes)
 		{
 			bytes.ThrowIfNull(nameof(bytes));
-			if (bytes.Length % _charSize != 0) throw new ArgumentException($"Invalid {nameof(bytes)} length");
+			if (bytes.Length % CharSize != 0) throw new ArgumentException($"Invalid {nameof(bytes)} length");
 			if (bytes.Length == 0) return string.Empty;
 
 			fixed (byte* p = bytes)
-				return new string(new Span<char>(p, bytes.Length / _charSize));
+				return new string(new Span<char>(p, bytes.Length / CharSize));
 		}
 	}
 }
