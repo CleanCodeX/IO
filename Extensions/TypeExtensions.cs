dc3096a225391a95572e0debf70b450b12cfae46
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-02-14 17:23:53 +0100

    added extension and helper

--- Extensions/TypeExtensions.cs
+++ Extensions/TypeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 
@@ -7,9 +8,13 @@ namespace IO.Extensions
 {
 	public static class TypeExtensions
 	{
+		public static IEnumerable<FieldInfo> GetPublicInstanceFields([NotNull] this Type type) => type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+		public static IEnumerable<FieldInfo> GetPublicStaicFields([NotNull] this Type type) => type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+
 		public static IReadOnlyDictionary<string, T> GetPublicConstants<T>(this Type type) =>
 			type
-				.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+				.GetPublicStaicFields()
 				.Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(T))
 				.ToDictionary(k => k.Name, v => (T)v.GetRawConstantValue()!)!;
 	}
