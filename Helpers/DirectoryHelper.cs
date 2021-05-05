fb7b3c2d6a02354eaf00757aacf361c384385c54
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-05-05 23:26:32 +0200

    * added some more directory helper methods * added FileHelper

--- Helpers/DirectoryHelper.cs
+++ Helpers/DirectoryHelper.cs
@@ -8,7 +8,7 @@ namespace IO.Helpers
 	public static class DirectoryHelper
 	{
 		/// <summary>
-		/// Creates the file's directory if necessary.
+		/// Creates the file's directory if existing.
 		/// </summary>
 		/// <param name="filePath">The file's directory to check</param>
 		public static void EnsureFileDirectoryExists(in string filePath) => EnsureDirectoryExists(Path.GetDirectoryName(filePath)!);
@@ -19,8 +19,33 @@ namespace IO.Helpers
 		/// <param name="directoryPath">The directory to check</param>
 		public static void EnsureDirectoryExists(in string directoryPath)
 		{
-			if (!Directory.Exists(directoryPath))
-				Directory.CreateDirectory(directoryPath!);
+			if (Directory.Exists(directoryPath)) return;
+
+			Directory.CreateDirectory(directoryPath!);
+		}
+
+		/// <summary>
+		/// Emties the directory if necessary, otherwise creates it
+		/// </summary>
+		/// <param name="directoryPath">The directory to check</param>
+		public static void EnsureEmptyDirectoryExists(in string directoryPath)
+		{
+			EnsureDirectoryNotExists(directoryPath);
+			EnsureDirectoryExists(directoryPath);
+		}
+
+		
+
+		/// <summary>
+		/// Deletes the directory if existing.
+		/// </summary>
+		/// <param name="directoryPath">The directory to check</param>
+		/// <param name="recursive">Whether to delete the subdirectories recursively or not</param>
+		public static void EnsureDirectoryNotExists(in string directoryPath, bool recursive = true)
+		{
+			if (!Directory.Exists(directoryPath)) return;
+			
+			Directory.Delete(directoryPath, recursive);
 		}
 	}
 }
---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace IO.Helpers
+{
+	public static class FileHelper
+	{
+		/// <summary>
+		/// Deletes the file if existing.
+		/// </summary>
+		/// <param name="filePath">The file path to check</param>
+		public static void EnsureFileNotExists(in string filePath)
+		{
+			if (!File.Exists(filePath)) return;
+			
+			File.Delete(filePath);
+		}
+	}
+}
